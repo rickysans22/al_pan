02ea52c99a78a155beea10f45f3c552e4e15dc18
Ricky Sans <rickysans40@gmail.com>
2020-07-09 21:01:46 -0500

    al pan

---
+++ al_pan.py
@@ -0,0 +1,16 @@
+print ("Buenos dias")
+
+Name = input ("Cual es tu nombre? ")
+
+print("Hola!! "+ Name + " que bonito nombre ")
+
+hora = input ("oye "+ Name + "... a que horas sales al pan? ")
+       
+print ("Ok " + Name +" ,,ahi te veo " + hora)
+
+
+age = input ("y que edad tienes " + Name +  "? ")
+
+print (age + " es muy buena edad " + Name)
+
+
